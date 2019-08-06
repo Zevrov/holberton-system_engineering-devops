f94e4454b7720cb75d5132f65e8a8bdb3e978cbf
Wolf Hopkins <zevrov.719@gmail.com>
2019-08-05 22:07:52 -0400

    fixed typo

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # create a file in tmp
-file { 'temp':
+file { 'temporary file':
+	ensure  => file,
 	path    => '/tmp/holberton',
 	mode    => '0744',
-	owner   => 'www-data',
 	group   => 'www-data',
+	owner   => 'www-data',
 	content => 'I love Puppet'
 }
