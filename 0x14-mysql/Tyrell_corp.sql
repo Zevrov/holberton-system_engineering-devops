bb6bd8f23a47804f2b57cc128d781826012e7334
Wolf Hopkins <719@holbertonschool.com>
2019-09-05 17:35:47 -0400

    fixed the comment so the script works

--- 0x14-mysql/Tyrell_corp.sql
+++ 0x14-mysql/Tyrell_corp.sql
@@ -1,4 +1,4 @@
---script for the first project
+-- for the first project
 CREATE DATABASE IF NOT EXISTS tyrell_corp;
 use tyrell_corp;
 CREATE TABLE IF NOT EXISTS nexus6(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
