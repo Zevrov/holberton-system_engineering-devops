1989ff1279e735a6ecea7bc938a234cc42bfaf9c
Wolf Hopkins <719@holbertonschool.com>
2019-09-16 21:03:49 -0400

    finished task 3, testing vs checker

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module to connect to an api and grab data"""
+
+import collections
+import csv
+import json
+import requests
+from sys import argv
+import sys
+
+if __name__ == "__main__":
+    employeeRequest = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + argv[1])
+    employeeDict = employeeRequest.json()
+
+    todoRequest = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1])
+    todoList = todoRequest.json()
+
+    task_data = []
+    jsonData = collections.OrderedDict()
+
+    for person in employeeDict:
+        for thing in todoList:
+            if person.get("id") == thing.get("userId"):
+                taskDict = {
+                    "username": person.get("username"),
+                    "task": thing.get("title"),
+                    "completed": thing.get("completed"),}
+                taskData.append(taskDict)
+        jsonData["{}".format(person.get("id"))] = taskData
+        taskData = []
+
+    with open("todo_all_employees.json", "w") as people:
+        json.dump(jsonData, people)
