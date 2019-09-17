a90c1df8c07141a74289b48676ceed40cc7b7dfb
Wolf Hopkins <719@holbertonschool.com>
2019-09-16 20:29:06 -0400

    first task done, initial commit, created readme

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""pulls todo from a given employee id"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    employeeRequest = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + argv[1])
+    employeeDict = employeeRequest.json()
+
+    todoRequest = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1])
+    todoList = todoRequest.json()
+
+    employeeName = employeeDict.get("name")
+    numberOfTasks = len(todoList)
+    completedTasks = [
+        dict for dict in todoList if dict.get("completed") is True]
+
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        employeeName,
+        len(completedTasks),
+        numberOfTasks))
+
+    for completeTask in completedTasks:
+        print("\t {}".format(completeTask.get("title")))
