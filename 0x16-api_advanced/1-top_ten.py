4ea77830f3dc44020a1b48a7f8e6d9ec1e217747
Wolf Hopkins <719@holbertonschool.com>
2019-09-19 18:25:05 -0400

    not sure why this change isn't staged

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,11 +9,7 @@ def top_ten(subreddit):
     headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(path, allow_redirects=False, headers=headers)
     if response.status_code in (302, 404):
-<<<<<<< HEAD
         print("None")
-=======
-        print("none")
->>>>>>> a47dde110abc6393ba25f03c8bc3a53d1a32cc80
 
     json = response.json()
     json_children = json.get('data').get('children')
