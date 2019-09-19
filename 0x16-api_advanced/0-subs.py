bd54ef9c5ebbcaf38b6f647f481302b0029af7bd
Wolf Hopkins <719@holbertonschool.com>
2019-09-19 18:09:12 -0400

    fixed 0, my understanding of how to use headers was off [line 9 and 10 fixed]

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,8 @@ import requests
 def number_of_subscribers(subreddit):
     """return the number of subscribers from a subreddit"""
     path = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": user_agent}
-    response = requests.get(path, allow_redirects=False, headers=user_agent)
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(path, allow_redirects=False, headers=headers)
     if response.status_code in (302, 404):
         return 0
     json = response.json()
