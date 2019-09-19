b5e3a995f207151e5979e795d173c6255d7c2651
Wolf Hopkins <719@holbertonschool.com>
2019-09-19 18:03:15 -0400

    testing solution for task 0 against the checker

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-
+"""ask reddit api the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """return the number of subscribers from a subreddit"""
+    path = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": user_agent}
+    response = requests.get(path, allow_redirects=False, headers=user_agent)
+    if response.status_code in (302, 404):
+        return 0
+    json = response.json()
+    return json.get('data').get('subscribers')
